364736e520be7a6943be22774c0eb5587ce8c0d1
PaulKats <PaulKats297@gmail.com>
2026-05-28 15:38:41 -0700

    feat(protocol): add PairingConfirm and PairingReject message types

--- src/FileTransfer.Core/Protocol/MessageType.cs
+++ src/FileTransfer.Core/Protocol/MessageType.cs
@@ -3,6 +3,8 @@ namespace FileTransfer.Core.Protocol;
 public enum MessageType : byte
 {
     Hello = 0x01,
+    PairingConfirm = 0x02,
+    PairingReject = 0x03,
     Text = 0x10,
     FileOffer = 0x20,
     FileChunk = 0x21,
